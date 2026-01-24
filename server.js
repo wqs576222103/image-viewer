8400421a0763c73e5229e6e77f9e16eb80a13867
root <576222103@qq.com>
2026-01-25 00:13:48 +0800

    build: 设置图片资源缓存

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const PORT = process.env.PORT || 3000;
 // 中间件
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/file/uploads', express.static(path.join(__dirname, 'uploads')));
+// 设置缓存
+app.use('/file/uploads', express.static(path.join(__dirname, 'uploads'), { maxAge: 86400000 }));
 
 // 使用路由模块
 app.use('/api', imageRoutes);
